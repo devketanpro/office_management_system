9ca0894954b9745ca86098fd6c2fdfadf28bbcfa
ketan <devketanpro11@gmail.com>
2024-03-05 10:19:05 +0530

    signup for user

---
+++ users/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from users.models import User, USER_TYPE
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "email", "phone", "password", "first_name", "last_name")
+        extra_kwargs = {
+            "email": {"required": True, "allow_blank": False},
+            "phone": {"required": True, "allow_blank": False},
+            "password": {"required": True, "allow_blank": False},
+            "first_name": {"required": True, "allow_blank": False},
+            "last_name": {"required": True, "allow_blank": False},
+        }
+
+    def validate_email(self , value):
+        user = User.objects.filter(email=value.lower())
+        if user:
+            raise serializers.ValidationError("User with this email address already exists")
+        return value
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            **validated_data,
+            role=USER_TYPE.USER
+        )
+        return user
---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from users.views import SignUpApiView
+
+urlpatterns =[
+    path("sign-up/", SignUpApiView.as_view(), name="signup"),
+]
+# Generate token
+token_urls = [
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
+
+urlpatterns += token_urls
+
